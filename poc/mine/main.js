89797208fb4c51225b67cba3253ec3e14e4b2517
BabelDev0 <leonardo.straccali@gmail.com>
2023-03-08 15:22:46 +0100

    feat (poc): first step

---
+++ poc/mine/main.js
@@ -0,0 +1,47 @@
+import { RLN, Registry, Cache, genExternalNullifier } from 'rlnjs';
+import * as path from 'path';
+import * as fs from 'fs';
+
+// This assumes you have built the circom circuits and placed them into the folder ./zkeyFiles
+const zkeyFilesPath = './zkeyFiles';
+const vkeyPath = path.join(zkeyFilesPath, 'verification_key.json');
+const vKey = JSON.parse(fs.readFileSync(vkeyPath, 'utf-8'));
+const wasmFilePath = path.join(zkeyFilesPath, 'rln.wasm');
+const finalZkeyPath = path.join(zkeyFilesPath, 'rln_final.zkey');
+
+// Instantiate RLN
+const rlnInstance = new RLN(wasmFilePath, finalZkeyPath, vKey);
+
+// Example of accessing the generated identity commitment
+const identity = rlnInstance.identity;
+const identityCommitment = rlnInstance.commitment;
+
+console.log('Identity: ', identity);
+console.log('Identity commitment: ', identityCommitment);
+
+// generate RLN registry that contains slashed registry
+const registry = new Registry(); // using the default tree depth
+registry.addMember(identityCommitment);
+
+const cache = new Cache(rlnInstance.rlnIdentifier);
+
+// Example of generating a proof
+const epoch = genExternalNullifier('test-epoch');
+const signal = 'This is a test signal';
+
+const merkleProof = registry.generateMerkleProof(identityCommitment);
+
+const proof = await rlnInstance.generateProof(signal, merkleProof, epoch);
+
+let result = cache.addProof(proof);
+console.log(result.status); // "added" or "breach" or "invalid"
+
+const proofResult = await RLN.verifyProof(vKey, proof);
+console.log(proofResult); // true
+
+const signal2 = 'This is a test signal2';
+const proof2 = await rlnInstance.generateProof(signal2, merkleProof, epoch);
+result = cache.addProof(proof2);
+console.log(result); // "added" or "breach" or "invalid"
+
+console.log('DONE!');
