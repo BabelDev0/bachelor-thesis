27e2a4a1039bd77103932f495bd2a58ca0039962
BabelDev0 <leonardo.straccali@gmail.com>
2023-03-09 20:38:34 +0100

    move old stuff

--- rln-poc/dist/hasher.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const circomlib_1 = require("circomlib");
-function poseidonHash(inputs) {
-    return (0, circomlib_1.poseidon)(inputs);
-}
-exports.default = poseidonHash;
-//# sourceMappingURL=hasher.js.map
