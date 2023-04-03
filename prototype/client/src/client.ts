35e74426e71bd4decea2842e4d05bdb816b98e19
BabelDev0 <leonardo.straccali@gmail.com>
2023-04-03 19:45:57 +0200

    feat (core): change code to permit only 10 request from client

--- prototype/client/src/client.ts
+++ prototype/client/src/client.ts
@@ -1,4 +1,5 @@
 import { initRLN, register, options } from './api';
+
 const main = async () => {
 	try {
 		await initRLN();
@@ -6,8 +7,9 @@ const main = async () => {
 		const userRegistrationStatus = await register();
 		console.log('Server response: ', userRegistrationStatus);
 
-		for (; ;) {
-			const userOptions = await options();
+		for (let i = 0; i < 10; i++) {
+			const response = await options();
+			console.log('Server response: ', response);
 		}
 	} catch (error) {
 		throw new Error(error);
